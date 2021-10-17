d83b39ff7172ec2c45000373a6c413f63e2b2220
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-10-17 01:16:19 -0500

    valid subsequence exercise

---
+++ valid_subsequence.js
@@ -0,0 +1,26 @@
+
+/**
+ *  Verify if an array is the subsequence of another.
+ * @param {array} array 
+ * @param {array} sequence 
+ * @returns {boolean}
+ */
+function isValidSubsequence(array, sequence) {
+    const arrayN=array.length;
+        var seqIdx=0;
+        const sequenceN=sequence.length;
+        for(var i=0; i<arrayN;i++){
+             if(array[i]==sequence[seqIdx]){
+                 seqIdx++;
+             }
+            
+        }
+        return sequenceN===seqIdx;
+    }
+    // Do not edit the line below.
+    exports.isValidSubsequence = isValidSubsequence;
+    
+    const array=[5,1,22,25,6,-1,8,10];
+    const sequence=[1,6,-1,10];
+    const result=isValidSubsequence(array,sequence);
+    console.log(result);
