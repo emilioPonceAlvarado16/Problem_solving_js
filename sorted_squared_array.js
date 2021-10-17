577ad4e122d7898f0ec811fa7f44994f55cf20eb
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-10-17 01:29:29 -0500

    sorted squared array exercise

---
+++ sorted_squared_array.js
@@ -0,0 +1,21 @@
+
+/**Return a sorted squared array 
+ * 
+ * @param {array} array 
+ * @returns 
+ */
+
+function sortedSquaredArray(array) {
+    var output=[];
+   for(var i=0;i<array.length;i++){
+       output.push(array[i]**2);
+   }
+  
+   output.sort(function(a, b){return a-b});
+ return output;
+}
+
+exports.sortedSquaredArray = sortedSquaredArray;
+
+const results=sortedSquaredArray([1,8,5,8,9]);
+console.log(results);
